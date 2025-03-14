b9089d340df52fbc0a2211f1f94cd70b79488083
Nicola Ciancia <s322359@studenti.polito.it>
2025-03-14 16:12:36 +0100

    feat: added plugins and dependencies for csv an Uber-Jar

--- RouteAnalyzer/build.gradle.kts
+++ RouteAnalyzer/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     kotlin("jvm") version "2.1.10"
+    kotlin("plugin.serialization") version "2.1.10"
+    id("com.gradleup.shadow") version "8.3.6"
+    application
 }
 
 group = "polito.it.wa2.g04.lab01"
@@ -11,11 +14,23 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"]  = "com.polito.it.wa2.g04.lab01.MainKt"
+    }
+}
+
 kotlin {
     jvmToolchain(23)
+}
+
+application {
+    mainClass = "com.polito.it.wa2.g04.lab01.MainKt"
 }
--- RouteAnalyzer/src/main/kotlin/polito/it/wa2/g04/lab01/Main.kt
+++ RouteAnalyzer/src/main/kotlin/polito/it/wa2/g04/lab01/Main.kt
@@ -1,2 +1,8 @@
 package polito.it.wa2.g04.lab01
+import kotlinx.serialization.Serializable
+
+fun main() {
+    println("Hello, World!")
+
+}
 
