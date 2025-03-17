03158de446905570dab7a7c45bfed921ade843c7
Luca Pes <luca.pes.m@gmail.com>
2025-03-17 12:16:24 +0100

    feat: updated Dockerfile and added implementation for countWaypointsOutsideGeofence

--- RouteAnalyzer/src/main/kotlin/it/polito/wa2/g04/services/RouteAnalyzerService.kt
+++ RouteAnalyzer/src/main/kotlin/it/polito/wa2/g04/services/RouteAnalyzerService.kt
@@ -1,22 +1,23 @@
 package it.polito.wa2.g04.services
 
+import kotlin.math.*
 import it.polito.wa2.g04.config.Config
 import it.polito.wa2.g04.models.Waypoint
 import it.polito.wa2.g04.models.Geofence
-import jdk.vm.ci.common.JVMCIError.unimplemented
-import kotlin.math.*
 
 class RouteAnalyzerService(private val config: Config) {
     fun calculateMaxDistanceFromStart(waypoints: List<Waypoint>): Double {
-        throw unimplemented()
+        return 1.0
     }
 
     fun findMostFrequentedArea(waypoints: List<Waypoint>): Waypoint {
-        throw unimplemented()
+        return Waypoint(1.0, 1.0, 1.0)
     }
 
-    fun countWaypointsOutsideGeofence(waypoints: List<Waypoint>, geofence: Geofence): Int {
-        throw unimplemented()
+    fun countWaypointsOutsideGeofence(waypoints: List<Waypoint>, geofence: Geofence): List<Waypoint> {
+        return waypoints.filter {
+            haversine(it.lat, it.lng, geofence.centerLat, geofence.centerLng) > geofence.radiusKm
+        }
     }
 
     private fun haversine(lat1: Double, lng1: Double, lat2: Double, lng2: Double): Double {
@@ -27,7 +28,9 @@ class RouteAnalyzerService(private val config: Config) {
         val deltaLngRadians = Math.toRadians(lng2 - lng1)
 
         val a =
-            sin(deltaLatRadians / 2) * sin(deltaLatRadians / 2) + cos(lat1) * cos(lat2) * sin(deltaLngRadians / 2) * sin(
+            sin(deltaLatRadians / 2) * sin(deltaLatRadians / 2) + cos(lat1Radians) * cos(lat2Radians) * sin(
+                deltaLngRadians / 2
+            ) * sin(
                 deltaLngRadians / 2
             )
         val c = 2 * atan2(sqrt(a), sqrt(1 - a))
